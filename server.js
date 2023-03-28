848761c78ee4ab654c84f3b7546bdeb62c4e1a57
gp0710 <gabrielle.pan@baruchmail.cuny.edu>
2023-03-28 13:40:55 -0400

    change server.js express.static('public') to express.static('dist')

--- server.js
+++ server.js
@@ -7,10 +7,8 @@ const axios = require('axios')
 const express = require('express')
 const app = express()
 
-const ghpages = require('gh-pages');
-
 app.use(express.json())
-app.use(express.static('public'))
+app.use(express.static('dist'))
 
 app.post('/weather', (req, res) => {
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${req.body.latitude}&lon=${req.body.longitude}&appid=${OPENWEATHER_API_KEY}&units=metric`
